1db8a1fbf56c8a02f68c99cb061c68566e3845f6
Mikalai Alkhimenka <alkhimenkamikalai90@gmail.com>
2023-08-17 00:33:35 +0200

    Solved

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,37 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
 
+    public String getColor() {
+        return color;
+    }
+
+    public int getNumberOfPaws() {
+        return numberOfPaws;
+    }
+
+    public boolean isHasFur() {
+        return hasFur;
+    }
+
+    public Animal(String color, int numberOfPaws, boolean hasFur) {
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription() {
+        String fur = hasFur ? "a" : "no";
+        String paw = numberOfPaws == 1 ? "paw" : "paws";
+        return "This animal is mostly " + color + ". It has " + numberOfPaws + " " + paw + " and " + fur + " fur.";
+    }
 }
+
+
+
+
+
+
--- src/main/java/com/epam/mjc/stage0/Bird.java
+++ src/main/java/com/epam/mjc/stage0/Bird.java
@@ -1,5 +1,14 @@
 package com.epam.mjc.stage0;
 
-public class Bird {
+public class Bird extends Animal{
+    public Bird() {
+        super("blue", 2, false);
+    }
+    @Override
+    public String getDescription() {
+        String fur = isHasFur() ? "a" : "no";
+        String paw = getNumberOfPaws() == 1 ? "paw" : "paws";
+        return "This animal is mostly " + getColor() + ". It has " + getNumberOfPaws() + " " + paw + " and " + fur + " fur. Moreover, it has 2 wings and can fly.";
+    }
 
 }
